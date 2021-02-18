c0530d2e4daa326fd755ca8aa33ee2eb701878f7
Berjis <berjistechnologies@gmail.com>
2021-02-18 14:41:48 +0300

    Wallet

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -30,10 +30,12 @@
     </div>
     <div class="col-sm-4">
         <div class="row">
-            <div class="col-sm-12 balance-card">
-                <span class="balance-title">Account Balance</span>
-                <span class="balance-amount">$25,386</span>
-                <span class="balance-account">Primary</span>
+            <div class="col-sm-12">
+                <div class="balance-card">
+                    <span class="balance-title">Account Balance</span>
+                    <span class="balance-amount">$25,386</span>
+                    <span class="balance-account">Primary</span>
+                </div>
             </div>
         </div>
     </div>
