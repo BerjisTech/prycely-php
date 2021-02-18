1e986a4db83d437db9d4024baede4cdc28038578
Berjis <berjistechnologies@gmail.com>
2021-02-18 12:02:25 +0300

    Bootstrap and JQuery

---
+++ app/Views/wallet.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col-sm-4">
+        row
+    </div>
+    <div class="col-sm-4">
+        row
+    </div>
+    <div class="col-sm-4">
+        row
+    </div>
+</div>
