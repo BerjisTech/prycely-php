8afe46081282d0182eac6e157fded946a60a5e77
Berjis <berjistechnologies@gmail.com>
2021-02-18 15:52:21 +0300

    Wallet

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -23,7 +23,7 @@
             <div class="col-sm-6 transactions-card">
                 <p>Transactions</p>
                 <p>15th February, 2021</p>
-                <table class="table table-responsive transaction-table">
+                <table class="table transaction-table">
                     <tr>
                         <td>
                             <p class="transaction-image" style="background: url(https://fraser-pryce.herokuapp.com/assets/images/36471e6f2cbcccac675f875ef51b3bda.webp);"></p>
