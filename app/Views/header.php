7c8f8aa9080611b52d5a6613042c1e108300a8e6
Berjis <berjistechnologies@gmail.com>
2021-02-18 11:53:13 +0300

    Bootstrap and JQuery

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css?stamp='.time()); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.mobile.css?stamp='.time()); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-grid.css'); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-reboot.css'); ?>" />
