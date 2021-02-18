48dfbfee70d73e7d74eddb7a3f9d022fc0463481
Berjis <berjistechnologies@gmail.com>
2021-02-18 15:32:54 +0300

    Wallet

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -21,7 +21,25 @@
 
         <div class="row">
             <div class="col-sm-6">
-                <p>row</p>
+                <p>Transactions</p>
+                <p>15th February, 2021</p>
+                <table class="transaction-table">
+                    <tr>
+                        <td>
+                            <span class="transaction-image" style="background: url(https://fraser-pryce.herokuapp.com/assets/images/36471e6f2cbcccac675f875ef51b3bda.webp);"></span>
+                        </td>
+                        <td class="transaction-details">
+                            <p>
+                                <span class="transaction-title">Netflix</span>
+                                <span class="transaction-amount">13,000</span>
+                            </p>
+                            <p>
+                                <span class="transaction-status">Netflix</span>
+                                <span class="transaction-time">13,000</span>
+                            </p>
+                        </td>
+                    </tr>
+                </table>
             </div>
             <div class="col-sm-6">
                 <div>row</div>
