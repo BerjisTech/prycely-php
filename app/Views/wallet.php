c66d3f9df23cbb5d26ad3038d9193ab85539e507
Berjis <berjistechnologies@gmail.com>
2021-02-18 13:40:10 +0300

    Bootstrap and JQuery

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -1,11 +1,11 @@
 <div class="row">
     <div class="col-sm-8">
-        <div class="col-sm-12">
+        <div class="row">
             <h3>Overview</h3>
             <small>Hi Fraser, some unneccesary greetings text here</small>
         </div>
 
-        <div class="col-sm-12 slide-icons-parent">
+        <div class="row slide-icons-parent">
             <div class="slide-icons">
                 <img src="https://fraser-pryce.herokuapp.com/assets/images/36471e6f2cbcccac675f875ef51b3bda.webp" style="margin: 5px; height: 50px" />
                 <img src="https://fraser-pryce.herokuapp.com/assets/images/36471e6f2cbcccac675f875ef51b3bda.webp" style="margin: 5px; height: 50px" />
