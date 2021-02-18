9e9223a489ead298735bc703ea1ef16891fb3d08
Berjis <berjistechnologies@gmail.com>
2021-02-18 12:11:42 +0300

    Bootstrap and JQuery

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -1,18 +1,15 @@
 <div class="row">
-    <div class="col-sm-12">
-        <div class="pull-left">
+    <div class="col-sm-8">
+        <div class="col-sm-12">
             <h3>Overview</h3>
             <small>Hi Fraser, some unneccesary greetings text here</small>
         </div>
-        <div class="pull-left">
-            <img class="img img-responsive img-round" style="width: 100px;" src="<?php echo base_url('assets/images/36471e6f2cbcccac675f875ef51b3bda.webp'); ?>"/>
+        <div class="col-sm-6">
+            row
         </div>
-    </div>
-    <div class="col-sm-4">
-        row
-    </div>
-    <div class="col-sm-4">
-        row
+        <div class="col-sm-6">
+            row
+        </div>
     </div>
     <div class="col-sm-4">
         row
