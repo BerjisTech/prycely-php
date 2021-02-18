b75351b2264c69139a0e0e71d70dd1041b9265b6
Berjis <berjistechnologies@gmail.com>
2021-02-18 11:07:56 +0300

    Bootstrap and JQuery

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'fraser-pryce.herokuapp.com/';
+	public $baseURL = 'https://fraser-pryce.herokuapp.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
