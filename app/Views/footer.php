5c9e030a38b81f95eee073798f7ddb3658cb82a2
Berjis <berjistechnologies@gmail.com>
2021-02-18 11:32:26 +0300

    Bootstrap and JQuery

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,2 +1,2 @@
-<script src="<?php echo base_url('assets/js/pryce.js'); ?>"></script>
-<script src="<?php echo base_url('node_modules/jquery/dist/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/pryce.js?stamp='.time()); ?>"></script>
+<script src="<?php echo base_url('node_modules/jquery/dist/jquery.min.js?stamp='.time()); ?>"></script>
--- app/Views/header.php
+++ app/Views/header.php
@@ -1,1 +1,1 @@
-<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css'); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css?stamp='.time()); ?>" />
