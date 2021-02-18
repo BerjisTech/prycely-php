fb868e6ba6827edb0a35755c41bb1366f00fc636
Berjis <berjistechnologies@gmail.com>
2021-02-18 13:31:27 +0300

    Bootstrap and JQuery

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -5,7 +5,7 @@
             <small>Hi Fraser, some unneccesary greetings text here</small>
         </div>
 
-        <div class="col-sm-12">
+        <div class="col-sm-12 slide-icons-parent">
             <div class="slide-icons">
                 <img src="https://fraser-pryce.herokuapp.com/assets/images/36471e6f2cbcccac675f875ef51b3bda.webp" style="margin: 5px; height: 50px" />
                 <img src="https://fraser-pryce.herokuapp.com/assets/images/36471e6f2cbcccac675f875ef51b3bda.webp" style="margin: 5px; height: 50px" />
