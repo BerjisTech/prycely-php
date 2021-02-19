17bf4a87e468a257d0726388f36622f28fb5e0d1
Berjis <berjistechnologies@gmail.com>
2021-02-19 15:45:18 +0300

    Font awesome

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css?stamp='.time()); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.mobile.css?stamp='.time()); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('node_modules/font-awesome/css/font-awesome.css?stamp='.time()); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap.css'); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap-grid.css'); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap-reboot.css'); ?>" />
-<link rel="stylesheet" href="<?php echo base_url('node_modules/font-awesome/css/font-awesome.css?stamp='.time()); ?>" />
 
 
 <script src="<?php echo base_url('node_modules/jquery/dist/jquery.min.js?stamp='.time()); ?>"></script>
