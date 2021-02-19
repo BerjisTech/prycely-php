10cdf8ea318ca7d8f39a01669c81fceec84834f0
Berjis <berjistechnologies@gmail.com>
2021-02-19 15:32:10 +0300

    Wallet

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -125,7 +125,7 @@
                 </div>
 
                     <div class="cc visa">
-                        <svg width="100%">
+                        <svg style="width:100%; opacity: 0.5;">
                             <path d="M 0 0 C 50 50 250 0 300 87"></path>
                         </svg>
                         <div class="container">
@@ -143,7 +143,7 @@
                     </div>
 
                     <div class="cc mastercard">
-                        <svg width="100%">
+                        <svg style="width:100%; opacity: 0.5;">
                             <path d="M 0 0 C 50 50 250 0 300 87"></path>
                         </svg>
                         <div class="container">
