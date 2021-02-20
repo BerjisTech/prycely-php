d3af45453f3c2b2f3f1fa98b9dced5a6cba044ad
Berjis <berjistechnologies@gmail.com>
2021-02-20 22:21:07 +0300

    Tabs

--- assets/js/pryce.js
+++ assets/js/pryce.js
@@ -1,6 +1,6 @@
 console.log('Go pryce');
 
-document.querySelectorAll('.switch-tab').onclick = function () {
+$('.switch-tab').on('click', function () {
     console.log($(this));
     if ($(this).attr('class').includes('active')) {
         return;
@@ -8,4 +8,4 @@ document.querySelectorAll('.switch-tab').onclick = function () {
         $('.switch-tab').removeClass('active');
         $(this).addClass('active');
     }
-}
+})
