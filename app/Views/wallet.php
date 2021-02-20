5e5dcf8093f9e42bb74d7f3e35155bad8b1e848d
Berjis <berjistechnologies@gmail.com>
2021-02-20 16:43:11 +0300

    Tabs

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -20,9 +20,9 @@
         </div>
 
         <div class="row row-tabs">
-            <div class="col-4 text-center">Transactions</div>
-            <div class="col-4 text-center">Stats</div>
-            <div class="col-4 text-center">Wallets</div>
+            <div class="col-4 text-center"><span>Transactions</span></div>
+            <div class="col-4 text-center"><span>Stats</span></div>
+            <div class="col-4 text-center"><span>Wallets</span></div>
         </div>
 
         <div class="row">
