e9d0927eb6258cb688d0aa25eb045716f01aca81
Berjis <berjistechnologies@gmail.com>
2021-02-22 21:17:52 +0300

    OnBoarding

---
+++ app/Controllers/OnBoarding.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class OnBoarding extends BaseController
+{
+    public function index()
+    {
+        return view('onboarding');
+    }
+}
---
+++ app/Views/onboarding.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Wallet</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+    <?php include('header.php'); ?>
+</head>
+
+<body>
+    <?php include('nav.php'); ?>
+    <div class="main-content">
+        <?php include($page_name . '.php'); ?>
+    </div>
+    <?php include('modals.php'); ?>
+    <?php include('footer.php'); ?>
+
+</body>
+
+</html>
