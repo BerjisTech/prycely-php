842587b9ba481009072283c554b44b9d7be6a098
Berjis <berjistechnologies@gmail.com>
2021-02-25 14:48:14 +0300

    Create account

--- app/Views/create_account.php
+++ app/Views/create_account.php
@@ -29,57 +29,56 @@
                     </div>
                     <div class="col-md-4 col-sm-5 col-6">
                         <div class="row">
-                            <div class="col-5 text-right"><a href="">Log In</a></div>
-                            <div class="col-7 text-center">
-                                <a href="#">English <span class="fa fa-chevron-down"></span></a>
-                            </div>
+                            <a href="">Log In</a>
+                            <a href="#">English <span class="fa fa-chevron-down"></span></a>
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 yourself">
-                        <h3>Tell us about yourself</h3>
-                        <div class="user_type">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
-                                        <td class="user-type">Company<span>Best suited for Companies</span></td>
-                                        <td><span class="fa fa-chevron-right"></span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="user_type">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
-                                        <td class="user-type">Freelancer<span>Best suited for Freelancers</span></td>
-                                        <td><span class="fa fa-chevron-right"></span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="user_type">
-                            <table>
-                                <tbody>
-                                    <tr>
-                                        <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
-                                        <td class="user-type">Individual<span>Best suited for Individuals and Personal use</span></td>
-                                        <td><span class="fa fa-chevron-right"></span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <span class="go_back"><span class="fal fa fa-arrow-left"></span> Go Back</span>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 hidden-xs right_home_panel">
-                <span style="background: url(https://miro.medium.com/max/1838/1*_KZKYMI2hlndOSGL02PK9w.gif);"></span>
+            <div class="row">
+                <div class="col-12 yourself">
+                    <h3>Tell us about yourself</h3>
+                    <div class="user_type">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
+                                    <td class="user-type">Company<span>Best suited for Companies</span></td>
+                                    <td><span class="fa fa-chevron-right"></span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="user_type">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
+                                    <td class="user-type">Freelancer<span>Best suited for Freelancers</span></td>
+                                    <td><span class="fa fa-chevron-right"></span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="user_type">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
+                                    <td class="user-type">Individual<span>Best suited for Individuals and Personal use</span></td>
+                                    <td><span class="fa fa-chevron-right"></span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <span class="go_back"><span class="fal fa fa-arrow-left"></span> Go Back</span>
+                </div>
             </div>
         </div>
+        <div class="col-sm-4 hidden-xs right_home_panel">
+            <span style="background: url(https://miro.medium.com/max/1838/1*_KZKYMI2hlndOSGL02PK9w.gif);"></span>
+        </div>
+    </div>
     </div>
 
     <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.js'); ?>"></script>
