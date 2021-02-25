dd53a5eddd9945b38c0abd793e39ac31a8aa05e2
Berjis <berjistechnologies@gmail.com>
2021-02-25 12:50:42 +0300

    Create account

--- app/Views/create_account.php
+++ app/Views/create_account.php
@@ -37,33 +37,39 @@
                 <div class="row">
                     <div class="col-12 yourself">
                         <h3>Tell us about yourself</h3>
-                        <table class="user_type">
-                            <tbody>
-                                <tr>
-                                    <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
-                                    <td class="user-type">Company<span>Best suited for Companies</span></td>
-                                    <td><span class="fa fa-chevron-right"></span></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <table class="user_type">
-                            <tbody>
-                                <tr>
-                                    <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
-                                    <td class="user-type">Freelancer<span>Best suited for Freelancers</span></td>
-                                    <td><span class="fa fa-chevron-right"></span></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <table class="user_type">
-                            <tbody>
-                                <tr>
-                                    <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
-                                    <td class="user-type">Individual<span>Best suited for Individuals and Personal use</span></td>
-                                    <td><span class="fa fa-chevron-right"></span></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div class="user_type">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
+                                        <td class="user-type">Company<span>Best suited for Companies</span></td>
+                                        <td><span class="fa fa-chevron-right"></span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="user_type">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
+                                        <td class="user-type">Freelancer<span>Best suited for Freelancers</span></td>
+                                        <td><span class="fa fa-chevron-right"></span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="user_type">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td><img src="<?php echo base_url('assets/images/logo.png') ?>" /></td>
+                                        <td class="user-type">Individual<span>Best suited for Individuals and Personal use</span></td>
+                                        <td><span class="fa fa-chevron-right"></span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
