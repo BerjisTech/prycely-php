5cfef03f02af35cfb3b63b6bba1f5ca53818b086
Berjis <berjistechnologies@gmail.com>
2021-02-25 20:21:21 +0300

    Create account

---
+++ app/Controllers/Internals.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Internals extends BaseController{
+
+    public function __construct()
+    {
+    
+    }
+
+    public function index(){
+        
+    }
+}
--- assets/js/pryce.js
+++ assets/js/pryce.js
@@ -1,3 +1,7 @@
+const { data } = require("jquery");
+
+let base_url = 'https://' + window.location.hostname;
+
 console.log('Go pryce');
 
 $('.switch-tab').on('click', function () {
@@ -7,4 +11,14 @@ $('.switch-tab').on('click', function () {
         $('.switch-tab').removeClass('active');
         $(this).addClass('active');
     }
+})
+
+$('a').on('click', function () {
+    $.ajax({
+        url: base_url + 'internals/link_stats',
+        data: link_data,
+        method: 'POST',
+        success: function () { },
+        error: function () { }
+    });
 })
