f0124bc4342f79ba5a93c5d146f04d5fe3a1518c
Berjis <berjistechnologies@gmail.com>
2021-02-25 12:32:20 +0300

    Create account

--- app/Controllers/CreateAccount.php
+++ app/Controllers/CreateAccount.php
@@ -5,11 +5,6 @@ namespace App\Controllers;
 class Createaccount extends BaseController
 {
 
-    public function __construct()
-    {
-        $this->load->library('session');
-    }
-
     public function index()
     {
         return view('create_account');
