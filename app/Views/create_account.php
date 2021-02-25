380b34fba16bd5a68b11034da56a9d784e6044d5
Berjis <berjistechnologies@gmail.com>
2021-02-25 20:26:30 +0300

    Create account

--- app/Views/create_account.php
+++ app/Views/create_account.php
@@ -80,7 +80,7 @@
             </div>
         </div>
     </div>
-
+    <script src="<?php echo base_url('assets/js/pryce.js?stamp=' . time()); ?>"></script>
     <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.js'); ?>"></script>
     <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.js'); ?>"></script>
 
