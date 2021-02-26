466932bd5df3ca50dded650556d851887a37eece
Berjis <berjistechnologies@gmail.com>
2021-02-26 16:41:47 +0300

    Create account

--- app/Views/create_account.php
+++ app/Views/create_account.php
@@ -70,7 +70,7 @@
                         </div>
                         <span class="go_back"><a href="<?php echo base_url() ?>"><span class="fal fa fa-arrow-left"></span> Go Back</a></span>
                     </div>
-                    <div class="col-12 the_email">
+                    <div class="col-12 the_email" style="display: none;">
                         <h3 class="the_title">Enter your email address</h3>
                         <span class="the_subtitle">You'll need to log in and access your account</span>
                         <div class="the_email_input">
@@ -78,6 +78,17 @@
                             <input autocomplete="FALSE" type="email" placeholder="you@mail.com" class="form_control" />
                             <button type="submit">NEXT <span class="fal fa fa-arrow-next"></span></button>
                             <span class="agreement">By clicking on submit you agree to out <a href="<?php echo base_url('privacy_policy'); ?>">Privacy Policy</a> applicable to the processing of your account</span>
+                        </div>
+                        <span class="go_back"><a href="#" data-step="2"><span class="fal fa fa-arrow-left"></span> Go Back</a></span>
+                    </div>
+                    <div class="col-12 the_email">
+                        <h3 class="the_title">Your confirmation code is waiting for you in your email box!</h3>
+                        <span class="the_subtitle">Enter the 6-digit confirmation code we've sent you at <span class="email_with_code"></span> to confirm your email address</span>
+                        <div class="the_email_input">
+                            <span class="the_email_input_title">Confirmation Code</span>
+                            <input autocomplete="FALSE" type="email" placeholder="XXX XXX" class="form_control" />
+                            <span class="resend_code"><span class="fal fa fa-info"></span> Didn't get any code? <a href="<?php echo base_url('privacy_policy'); ?>">Send a new code</a></span>
+                            <button type="submit">Confirm <span class="fal fa fa-arrow-next"></span></button>
                         </div>
                         <span class="go_back"><a href="#" data-step="2"><span class="fal fa fa-arrow-left"></span> Go Back</a></span>
                     </div>
