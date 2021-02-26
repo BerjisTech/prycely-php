a538f4503014d01bbe73a7b53a15198e811d9f42
Berjis <berjistechnologies@gmail.com>
2021-02-26 14:49:33 +0300

    Create account

--- app/Controllers/Createaccount.php
+++ app/Controllers/Createaccount.php
@@ -6,7 +6,7 @@ class Createaccount extends BaseController
 {
     public function index()
     {
-        print_r($this->db->query('SELECT * FROM shops'));
+        // print_r($this->db->query('SELECT * FROM shops'));
         return view('create_account');
     }
 }
