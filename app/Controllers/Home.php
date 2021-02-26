34d5cfcb99427124b47ee3aacd64637c659fbf43
Berjis <berjistechnologies@gmail.com>
2021-02-26 23:07:36 +0300

    Create account

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,6 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+
+	public function __construct()
+	{
+		$db      = \Config\Database::connect();
+		$builder = $db->table('users');
+	}
 	public function index()
 	{
 		return view('welcome_message');
@@ -16,6 +22,6 @@ class Home extends BaseController
 
 	public function dbtest()
 	{
-		print_r($this->db->query('SELECT * FROM shops'));
+		print_r($this->builder->query('SELECT * FROM shops'));
 	}
 }
