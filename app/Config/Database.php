09f5582e4f516dc14c586bb7a7753ecab20afc93
Berjis <berjistechnologies@gmail.com>
2021-02-26 13:53:12 +0300

    Create account

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,7 +33,7 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => 'https://134.209.242.145:3306',
-		'username' => 'root',
+		'username' => 'berjis',
 		'password' => '890Berjis*()',
 		'database' => 'sleek_upsell',
 		'DBDriver' => 'MySQLi',
