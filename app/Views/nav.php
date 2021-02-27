d59e62c53d514990e0cec335e0354e182d786bfc
Berjis <berjistechnologies@gmail.com>
2021-02-27 16:54:48 +0300

    Create account

--- app/Views/nav.php
+++ app/Views/nav.php
@@ -1,5 +1,6 @@
 <div class="side_bar">
     <a href="" class="nav_links"><span class="fa fa-home"></span><span class="nav_link_text">Home</span></a>
-    <a href="" class="nav_links"><span class="fa fa-home"></span><span class="nav_link_text">Wallets</span></a>
-    <a href="" class="nav_links"><span class="fa fa-home"></span><span class="nav_link_text">Groups</span></a>
+    <a href="" class="nav_links"><span class="fa fa-money"></span><span class="nav_link_text">Wallets</span></a>
+    <a href="" class="nav_links"><span class="fa fa-users"></span><span class="nav_link_text">Groups</span></a>
+    <a href="" class="nav_links"><span class="fa fa-cog"></span><span class="nav_link_text">Settings</span></a>
 </div>
