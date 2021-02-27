3aeb875347052623d80c7f623d38123fd0533ae7
Berjis <berjistechnologies@gmail.com>
2021-02-28 00:03:20 +0300

    Create account

--- app/Views/nav.php
+++ app/Views/nav.php
@@ -1,6 +1,5 @@
 <div class="side_bar">
     <a href="" class="user_icon"></a>
-    <hr />
     <a href="<?php if ($page_name != 'overview') {
                     echo base_url('overview');
                 } else {
