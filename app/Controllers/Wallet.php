bdab13be2dfe0504e4ce43346f351b8ff3d779a0
Berjis <berjistechnologies@gmail.com>
2021-02-27 16:21:24 +0300

    Create account

--- app/Controllers/Wallet.php
+++ app/Controllers/Wallet.php
@@ -6,19 +6,30 @@ class Wallet extends BaseController
 {
 	public function index()
 	{
-        $data['page_name'] = 'wallet';
+		$data['page_name'] = 'wallet';
 		return view('pryce', $data);
 	}
 
-	public function add(){
+	public function personal()
+	{
+	}
+
+	public function group($group_id = "")
+	{
+	}
+
+	public function add()
+	{
 		echo 'add wallet';
 	}
 
-	public function delete($wallet_id = ""){
+	public function delete($wallet_id = "")
+	{
 		echo 'delete wallet';
 	}
 
-	public function edit($wallet_id = ""){
+	public function edit($wallet_id = "")
+	{
 		echo 'edit wallet';
 	}
 }
--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -34,7 +34,7 @@
         </div>
 
         <div class="row">
-            <div class="col-sm-6 transactions-card wallet-panel left-card">
+            <div class="col-sm-12 transactions-card wallet-panel left-card">
                 <p>Transactions</p>
                 <p>15th February, 2021</p>
                 <table class="table transaction-table">
@@ -120,9 +120,6 @@
                         </td>
                     </tr>
                 </table>
-            </div>
-            <div class="col-sm-6 wallet-panel mid-card">
-                <div>row</div>
             </div>
         </div>
     </div>
