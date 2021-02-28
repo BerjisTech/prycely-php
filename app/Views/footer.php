5b9e90ac703d7aa2979c884effa360815a5dcdf4
Berjis <berjistechnologies@gmail.com>
2021-02-28 12:16:40 +0300

    jquery ui

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,8 +1,14 @@
 <script src="<?php echo base_url('assets/js/pryce.js?stamp='.time()); ?>"></script>
+
+<!-- BOOTSTRAP -->
 <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.js'); ?>"></script>
 <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.js'); ?>"></script>
+
+<!-- JQUERY -->
 <script src="<?php echo base_url('node_modules/jquery-ui-dist/external/jquery/jquery.js'); ?>"></script>
 <script src="<?php echo base_url('node_modules/jquery-ui-dist/jquery-ui.js'); ?>"></script>
-<script src="<?php echo base_url('node_modules/morris-js-module/morris.js'); ?>"></script>
-<script src="<?php echo base_url('node_modules/raphael/raphael.min.js'); ?>"></script>
+
+<!-- GRAPHS -->
 <script src="<?php echo base_url('node_modules/eve-raphael/eve.js'); ?>"></script>
+<script src="<?php echo base_url('node_modules/raphael/raphael.min.js'); ?>"></script>
+<script src="<?php echo base_url('node_modules/morris-js-module/morris.js'); ?>"></script>
--- app/Views/header.php
+++ app/Views/header.php
@@ -1,11 +1,17 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css?stamp='.time()); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.mobile.css?stamp='.time()); ?>" />
+
+<!-- FONT_AWESOME -->
 <link rel="stylesheet" href="<?php echo base_url('node_modules/font-awesome/css/font-awesome.css?stamp='.time()); ?>" />
+
+<!-- BOOTSTRAP -->
 <link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap.css'); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap-grid.css'); ?>" />
 <link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap-reboot.css'); ?>" />
-<link rel="stylesheet" href="<?php echo base_url('node_modules/jquery-ui-dist/jquery-ui.theme.min.css'); ?>" />
+
+<!-- GRAPHS -->
 <link rel="stylesheet" href="<?php echo base_url('node_modules/morris-js-module/morris.css'); ?>" />
 
-
+<!-- JQUERY -->
 <script src="<?php echo base_url('node_modules/jquery/dist/jquery.min.js?stamp='.time()); ?>"></script>
+<link rel="stylesheet" href="<?php echo base_url('node_modules/jquery-ui-dist/jquery-ui.theme.min.css'); ?>" />
