39aa664e864be3092de5516d54dfd4b012d4ff59
Berjis <berjistechnologies@gmail.com>
2021-02-28 21:54:09 +0300

    Wallet

--- app/Views/overview.php
+++ app/Views/overview.php
@@ -130,6 +130,49 @@
                     <span class="balance-title">Account Balance</span>
                     <span class="balance-amount">$25,386</span>
                 </div>
+                <p class="transaction-title">My Wallet</p>
+                <table class="table transaction-table table-hover">
+                    <tr>
+                        <td class="t-img">
+                            <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
+                        </td>
+                        <td class="transaction-details">
+                            <span class="transaction-title">KES - Local</span>
+                            <span class="transaction-status">Recipient and Sender</span>
+                        </td>
+                        <td class="transaction-td">
+                            <span class="transaction-amount complete">KES 230,000</span>
+                            <span class="transaction-time">KES 2,300</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="t-img">
+                            <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/us.svg'); ?>');"></p>
+                        </td>
+                        <td class="transaction-details">
+                            <span class="transaction-title">USD - Foreign</span>
+                            <span class="transaction-status">Recipient</span>
+                        </td>
+                        <td class="transaction-td">
+                            <span class="transaction-amount complete">$ 23,000</span>
+                            <span class="transaction-time">$ 200 pending</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="t-img">
+                            <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/gb.svg'); ?>');"></p>
+                        </td>
+                        <td class="transaction-details">
+                            <span class="transaction-title">GBP - Foreign</span>
+                            <span class="transaction-status">Recipient</span>
+                        </td>
+                        <td class="transaction-td">
+                            <span class="transaction-amount complete">GBP 23</span>
+                            <span class="transaction-time">GBP 500 pending</span>
+                        </td>
+                    </tr>
+                </table>
+                <span class="more_groups"><a href="<?php echo base_url('wallet'); ?>">View all wallets<span class="fal fa fa-arrow-right"></span></a></span>
 
                 <p class="transaction-title">My Groups</p>
                 <table class="table transaction-table table-hover">
