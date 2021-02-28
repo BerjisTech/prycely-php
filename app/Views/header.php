837ae8ab6340a7e86abd7bd1874f530ffaac9e7d
Berjis <berjistechnologies@gmail.com>
2021-02-28 12:32:20 +0300

    Neon

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,17 +1,22 @@
-<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css?stamp='.time()); ?>" />
-<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.mobile.css?stamp='.time()); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.css?stamp=' . time()); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('assets/css/pryce.mobile.css?stamp=' . time()); ?>" />
 
-<!-- FONT_AWESOME -->
-<link rel="stylesheet" href="<?php echo base_url('node_modules/font-awesome/css/font-awesome.css?stamp='.time()); ?>" />
+<link rel="stylesheet" href="<?php echo base_url('assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css'); ?>" id="style-resource-1">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/font-icons/entypo/css/entypo.css'); ?>" id="style-resource-2">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/font-icons/font-awesome/css/font-awesome.min.css'); ?>" id="style-resource-1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic" id="style-resource-3">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>" id="style-resource-4">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/neon-core.css'); ?>" id="style-resource-5">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/neon-theme.css'); ?>" id="style-resource-6">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/neon-forms.css'); ?>" id="style-resource-7">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css'); ?>" id="style-resource-8">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/skins/blue.css'); ?>" id="style-resource-9">
+                    <link rel="stylesheet" href="<?php echo base_url('assets/js/select2/select2-bootstrap.css'); ?>" id="style-resource-2">
+                    <link rel="stylesheet" href="<?php echo base_url('assets/js/select2/select2.css'); ?>" id="style-resource-3">
+                    <link rel="stylesheet" href="<?php echo base_url('assets/js/daterangepicker/daterangepicker-bs3.css'); ?>" id="style-resource-4">
 
-<!-- BOOTSTRAP -->
-<link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap.css'); ?>" />
-<link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap-grid.css'); ?>" />
-<link rel="stylesheet" href="<?php echo base_url('node_modules/bootstrap/dist/css/bootstrap-reboot.css'); ?>" />
+<script src="<?php echo base_url('assets/js/jquery-1.11.3.min.js'); ?>"></script>
 
-<!-- GRAPHS -->
-<link rel="stylesheet" href="<?php echo base_url('node_modules/morris-js-module/morris.css'); ?>" />
-
-<!-- JQUERY -->
-<script src="<?php echo base_url('node_modules/jquery/dist/jquery.min.js?stamp='.time()); ?>"></script>
-<link rel="stylesheet" href="<?php echo base_url('node_modules/jquery-ui-dist/jquery-ui.theme.min.css'); ?>" />
+<!--[if lt IE 9]><script src="<?php echo base_url(); ?>/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]> <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script> <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> <![endif]-->
