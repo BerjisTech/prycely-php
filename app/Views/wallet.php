c49d943a232e8b6e2089710119afb72197c60105
Berjis <berjistechnologies@gmail.com>
2021-02-28 21:36:44 +0300

    Neon

---
+++ app/Views/wallet.php
@@ -0,0 +1,32 @@
+<div class="row">
+    <p>My Wallets</p>
+    <table class="table transaction-table table-hover">
+        <tr>
+            <td class="t-img">
+                <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
+            </td>
+            <td class="transaction-details">
+                <span class="transaction-title">KES - Local</span>
+                <span class="transaction-status">Recipient and Sender</span>
+            </td>
+        </tr>
+        <tr>
+            <td class="t-img">
+                <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/us.svg'); ?>');"></p>
+            </td>
+            <td class="transaction-details">
+                <span class="transaction-title">USD - Foreign</span>
+                <span class="transaction-status">Recipient</span>
+            </td>
+        </tr>
+        <tr>
+            <td class="t-img">
+                <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/uk.svg'); ?>');"></p>
+            </td>
+            <td class="transaction-details">
+                <span class="transaction-title">GBP - Foreign</span>
+                <span class="transaction-status">Recipient</span>
+            </td>
+        </tr>
+    </table>
+</div>
