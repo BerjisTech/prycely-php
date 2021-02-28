c634e9944c92137b7abd07d63802ee4317bf308d
Berjis <berjistechnologies@gmail.com>
2021-02-28 21:48:52 +0300

    Wallet

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -1,44 +1,57 @@
 <div class="row">
-    <p>My Wallets</p>
-    <table class="table transaction-table table-hover">
-        <tr>
-            <td class="t-img">
-                <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
-            </td>
-            <td class="transaction-details">
-                <span class="transaction-title">KES - Local</span>
-                <span class="transaction-status">Recipient and Sender</span>
-            </td>
-            <td class="transaction-td">
-                <span class="transaction-amount complete">KES 230,000</span>
-                <span class="transaction-time">KES 2,300</span>
-            </td>
-        </tr>
-        <tr>
-            <td class="t-img">
-                <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/us.svg'); ?>');"></p>
-            </td>
-            <td class="transaction-details">
-                <span class="transaction-title">USD - Foreign</span>
-                <span class="transaction-status">Recipient</span>
-            </td>
-            <td class="transaction-td">
-                <span class="transaction-amount complete">$ 23,000</span>
-                <span class="transaction-time">$ 200 pending</span>
-            </td>
-        </tr>
-        <tr>
-            <td class="t-img">
-                <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/gb.svg'); ?>');"></p>
-            </td>
-            <td class="transaction-details">
-                <span class="transaction-title">GBP - Foreign</span>
-                <span class="transaction-status">Recipient</span>
-            </td>
-            <td class="transaction-td">
-                <span class="transaction-amount complete">GBP 23</span>
-                <span class="transaction-time">GBP 500 pending</span>
-            </td>
-        </tr>
-    </table>
+
+    <div class="row top">
+        <div class="col-md-8 col-sm-7 col-xs-6 transaction-title">
+            My Wallets
+        </div>
+        <div class="col-md-4 col-sm-5 col-xs-6 text-right">
+            <a href="#">Create New Wallet <span class="entypo-plus"></span></a>
+        </div>
+    </div>
+
+    <div class="col-sm-10">
+        <table class="table transaction-table table-hover">
+            <tr>
+                <td class="t-img">
+                    <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
+                </td>
+                <td class="transaction-details">
+                    <span class="transaction-title">KES - Local</span>
+                    <span class="transaction-status">Recipient and Sender</span>
+                </td>
+                <td class="transaction-td">
+                    <span class="transaction-amount complete">KES 230,000</span>
+                    <span class="transaction-time">KES 2,300</span>
+                </td>
+            </tr>
+            <tr>
+                <td class="t-img">
+                    <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/us.svg'); ?>');"></p>
+                </td>
+                <td class="transaction-details">
+                    <span class="transaction-title">USD - Foreign</span>
+                    <span class="transaction-status">Recipient</span>
+                </td>
+                <td class="transaction-td">
+                    <span class="transaction-amount complete">$ 23,000</span>
+                    <span class="transaction-time">$ 200 pending</span>
+                </td>
+            </tr>
+            <tr>
+                <td class="t-img">
+                    <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/gb.svg'); ?>');"></p>
+                </td>
+                <td class="transaction-details">
+                    <span class="transaction-title">GBP - Foreign</span>
+                    <span class="transaction-status">Recipient</span>
+                </td>
+                <td class="transaction-td">
+                    <span class="transaction-amount complete">GBP 23</span>
+                    <span class="transaction-time">GBP 500 pending</span>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div class="col-sm-2 hidden-xs"></div>
+
 </div>
