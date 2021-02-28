8f34bd5f955f1455bbdad0e3347385cbfe4c2afb
Berjis <berjistechnologies@gmail.com>
2021-02-28 13:11:49 +0300

    Neon

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -8,7 +8,7 @@
 <script src="<?php echo base_url('assets/js/bootstrap.js'); ?>" id="script-resource-3"></script>
 <!--script src="<?php echo base_url('assets/js/joinable.js'); ?>" id="script-resource-4"></script-->
 <!--script src="<?php echo base_url('assets/js/resizeable.js'); ?>" id="script-resource-5"></script-->
-<script src="<?php echo base_url('assets/js/neon-api.js'); ?>" id="script-resource-6"></script>
+<!-- <script src="<?php echo base_url('assets/js/neon-api.js'); ?>" id="script-resource-6"></script> -->
 <!-- script src="<?php echo base_url('assets/js/cookies.min.js'); ?>" id="script-resource-7"></script-->
 <script src="<?php echo base_url('assets/js/jvectormap/jquery-jvectormap-1.2.2.min.js'); ?>" id="script-resource-8"></script>
 <script src="<?php echo base_url('assets/js/jvectormap/jquery-jvectormap-europe-merc-en.js'); ?>" id="script-resource-9"></script>
@@ -18,10 +18,10 @@
 <script src="<?php echo base_url('assets/js/raphael-min.js'); ?>" id="script-resource-13"></script>
 <script src="<?php echo base_url('assets/js/morris.min.js'); ?>" id="script-resource-14"></script>
 <script src="<?php echo base_url('assets/js/toastr.js'); ?>" id="script-resource-15"></script>
-<script src="<?php echo base_url('assets/js/neon-chat.js'); ?>" id="script-resource-16"></script>
+<!-- <script src="<?php echo base_url('assets/js/neon-chat.js'); ?>" id="script-resource-16"></script> -->
 <!-- JavaScripts initializations and stuff -->
-<script src="<?php echo base_url('assets/js/neon-custom.js'); ?>" id="script-resource-17"></script> <!-- Demo Settings -->
-<script src="<?php echo base_url('assets/js/neon-demo.js'); ?>" id="script-resource-18"></script>
+<!-- <script src="<?php echo base_url('assets/js/neon-custom.js'); ?>" id="script-resource-17"></script> Demo Settings -->
+<!-- <script src="<?php echo base_url('assets/js/neon-demo.js'); ?>" id="script-resource-18"></script> -->
 <!--script src="<?php echo base_url('assets/js/neon-skins.js'); ?>" id="script-resource-19"></script-->
 <script src="<?php echo base_url('assets/js/select2/select2.min.js'); ?>" id="script-resource-9"></script>
 <script src="<?php echo base_url('assets/js/bootstrap-datepicker.js'); ?>" id="script-resource-12"></script>
