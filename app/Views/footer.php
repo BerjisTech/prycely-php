54cb21a3b6af7ad397e686a4faeb2871ad55b50d
Berjis <berjistechnologies@gmail.com>
2021-02-28 12:10:25 +0300

    jquery ui

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -3,3 +3,6 @@
 <script src="<?php echo base_url('node_modules/bootstrap/dist/js/bootstrap.bundle.js'); ?>"></script>
 <script src="<?php echo base_url('node_modules/jquery-ui-dist/external/jquery/jquery.js'); ?>"></script>
 <script src="<?php echo base_url('node_modules/jquery-ui-dist/jquery-ui.js'); ?>"></script>
+<script src="<?php echo base_url('node_modules/morris-js-module/morris.js'); ?>"></script>
+<script src="<?php echo base_url('node_modules/raphael/raphael.min.js'); ?>"></script>
+<script src="<?php echo base_url('node_modules/eve-raphael/eve.js'); ?>"></script>
