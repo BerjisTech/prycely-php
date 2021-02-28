f877c654c17a01b40537ef28aa588098ef7acb91
Berjis <berjistechnologies@gmail.com>
2021-02-28 21:40:01 +0300

    Wallet

--- app/Views/wallet.php
+++ app/Views/wallet.php
@@ -9,6 +9,10 @@
                 <span class="transaction-title">KES - Local</span>
                 <span class="transaction-status">Recipient and Sender</span>
             </td>
+            <td class="transaction-td">
+                <span class="transaction-amount complete">KES 230,000</span>
+                <span class="transaction-time">KES 2,300</span>
+            </td>
         </tr>
         <tr>
             <td class="t-img">
@@ -18,6 +22,10 @@
                 <span class="transaction-title">USD - Foreign</span>
                 <span class="transaction-status">Recipient</span>
             </td>
+            <td class="transaction-td">
+                <span class="transaction-amount complete">$ 23,000</span>
+                <span class="transaction-time">$ 200 pending</span>
+            </td>
         </tr>
         <tr>
             <td class="t-img">
@@ -26,6 +34,10 @@
             <td class="transaction-details">
                 <span class="transaction-title">GBP - Foreign</span>
                 <span class="transaction-status">Recipient</span>
+            </td>
+            <td class="transaction-td">
+                <span class="transaction-amount complete">GBP 23</span>
+                <span class="transaction-time">GBP 500 pending</span>
             </td>
         </tr>
     </table>
