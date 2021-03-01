0b7ffa7554dd3eb672b740176eee7c09ca1d321f
Berjis <berjistechnologies@gmail.com>
2021-03-01 16:20:08 +0300

    Wallet

--- app/Views/group.php
+++ app/Views/group.php
@@ -78,7 +78,7 @@
     <div class="col-sm-4 hidden-xs right-card switch-card">
         <div class="input-group">
             <input type="search" placholder="Search groups by name" class="form-control" />
-            <em class="input-group-addon entypo-search"></em>
+            <span class="input-group-addon entypo-search"></span>
         </div>
     </div>
 
