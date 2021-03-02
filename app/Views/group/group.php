a171b5af4a76e4caa093a41cddfd9f83df40a235
Berjis <berjistechnologies@gmail.com>
2021-03-02 23:55:00 +0300

    MPesa

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -76,36 +76,7 @@
             </div>
 
 
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="group-navi">
-                        <div class="group-entypo col-sm-3 col-xs-6">
-                            <div class="entypo-inner text-center">
-                                <span class="entypo-logout"></span>
-                                <span class="hidden-xs">Send Money</span>
-                            </div>
-                        </div>
-                        <div class="group-entypo col-sm-3 col-xs-6">
-                            <div class="entypo-inner text-center">
-                                <span class="entypo-doc-text"></span>
-                                <span class="hidden-xs">Statements</span>
-                            </div>
-                        </div>
-                        <div class="group-entypo col-sm-3 col-xs-6">
-                            <div class="entypo-inner text-center">
-                                <span class="entypo-publish"></span>
-                                <span class="hidden-xs">Top Up</span>
-                            </div>
-                        </div>
-                        <div class="group-entypo col-sm-3 col-xs-6">
-                            <div class="entypo-inner text-center">
-                                <span class="entypo-dot-3"></span>
-                                <span class="hidden-xs">More</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
 
             <div class="row">
                 <div class="col-sm-6 col-xs-12 text-center">
