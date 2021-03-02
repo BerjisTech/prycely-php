fbfb1cc26daa3f279dd4ce44217dec514c1a3dd9
Berjis <berjistechnologies@gmail.com>
2021-03-02 14:50:34 +0300

    MPesa

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -3,29 +3,29 @@
         <div class="row slide-icons-parent top">
             <div class="col-sm-12 slide-icons">
                 <div class="slide-icon">
-                    <span>
-                        <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
-                    </span>
+                    <p>
+                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                    </p>
                 </div>
                 <div class="slide-icon">
-                    <span>
-                        <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
-                    </span>
+                    <p>
+                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                    </p>
                 </div>
                 <div class="slide-icon">
-                    <span>
-                        <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
-                    </span>
+                    <p>
+                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                    </p>
                 </div>
                 <div class="slide-icon">
-                    <span>
-                        <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
-                    </span>
+                    <p>
+                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                    </p>
                 </div>
                 <div class="slide-icon">
-                    <span>
-                        <p class="transaction-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></p>
-                    </span>
+                    <p>
+                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                    </p>
                 </div>
             </div>
         </div>
