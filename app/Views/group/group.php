db31eda74f16b7c0768f29abb947fe17d93ac6b0
Berjis <berjistechnologies@gmail.com>
2021-03-02 23:11:16 +0300

    MPesa

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -8,9 +8,7 @@
             element: 'line-chart',
             data: [
                 <?php
-                for ($m = 30; $m > -1; $m-6) :
-                    $nowmonth = strtotime(date('d-M-Y', strtotime('-' . $m . ' days')));
-                    $lastmonth = strtotime(date('d-M-Y', strtotime('-' . ($m - 1) . ' days'))); ?> {
+                for ($m = 30; $m > -1; $m - 6) : ?> {
                         y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
                         a: getRandomInt(100, 0),
                         b: getRandomInt(100, 0)
