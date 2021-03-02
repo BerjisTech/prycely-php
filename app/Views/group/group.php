3b4f938c701913c2ac2b7db55cf8dcbb31b27836
Berjis <berjistechnologies@gmail.com>
2021-03-02 21:54:04 +0300

    MPesa

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -12,28 +12,30 @@
 
 
             <div class="col-xs-12 group-navi">
-                <div class="group-entypo col-sm-3 col-xs-6">
-                    <div class="entypo-inner text-center">
-                        <span class="entypo-logout"></span>
-                        <span class="hidden-xs">Send Money</span>
+                <div class="row">
+                    <div class="group-entypo col-sm-3 col-xs-6">
+                        <div class="entypo-inner text-center">
+                            <span class="entypo-logout"></span>
+                            <span class="hidden-xs">Send Money</span>
+                        </div>
                     </div>
-                </div>
-                <div class="group-entypo col-sm-3 col-xs-6">
-                    <div class="entypo-inner text-center">
-                        <span class="entypo-doc-text"></span>
-                        <span class="hidden-xs">Statements</span>
+                    <div class="group-entypo col-sm-3 col-xs-6">
+                        <div class="entypo-inner text-center">
+                            <span class="entypo-doc-text"></span>
+                            <span class="hidden-xs">Statements</span>
+                        </div>
                     </div>
-                </div>
-                <div class="group-entypo col-sm-3 col-xs-6">
-                    <div class="entypo-inner text-center">
-                        <span class="entypo-publish"></span>
-                        <span class="hidden-xs">Top Up</span>
+                    <div class="group-entypo col-sm-3 col-xs-6">
+                        <div class="entypo-inner text-center">
+                            <span class="entypo-publish"></span>
+                            <span class="hidden-xs">Top Up</span>
+                        </div>
                     </div>
-                </div>
-                <div class="group-entypo col-sm-3 col-xs-6">
-                    <div class="entypo-inner text-center">
-                        <span class="entypo-dot-3"></span>
-                        <span class="hidden-xs">More</span>
+                    <div class="group-entypo col-sm-3 col-xs-6">
+                        <div class="entypo-inner text-center">
+                            <span class="entypo-dot-3"></span>
+                            <span class="hidden-xs">More</span>
+                        </div>
                     </div>
                 </div>
             </div>
