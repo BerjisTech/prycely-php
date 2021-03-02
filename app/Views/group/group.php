d08fcd8328e76b01fe6f6c6962d30c67801ba85e
Berjis <berjistechnologies@gmail.com>
2021-03-02 17:55:25 +0300

    MPesa

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -1,37 +1,43 @@
 <div class="row">
     <div class="col-sm-8 left-card">
-        <div class="row slide-icons-parent">
-            <div class="col-sm-12 slide-icons">
-                <div class="slide-icon">
-                    <p>
-                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
-                    </p>
-                </div>
-                <div class="slide-icon">
-                    <p>
-                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
-                    </p>
-                </div>
-                <div class="slide-icon">
-                    <p>
-                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
-                    </p>
-                </div>
-                <div class="slide-icon">
-                    <p>
-                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
-                    </p>
-                </div>
-                <div class="slide-icon">
-                    <p>
-                        <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
-                    </p>
-                </div>
-            </div>
-        </div>
         <div class="row row-tabs hidden-lg hidden-sm hidden-md visible-xs-*">
             <span class="switch-tab active" data-hide="switch-transactions">Group</span>
             <span class="switch-tab" data-hide="switch-card">Chats</span>
+        </div>
+        <div class="switch-transactions">
+            <div class="">
+                <p class="transaction-title">Group Name</p>
+                <p>Group Goal</p>
+            </div>
+            <div class="row slide-icons-parent">
+                <div class="col-sm-12 slide-icons">
+                    <div class="slide-icon">
+                        <p>
+                            <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                        </p>
+                    </div>
+                    <div class="slide-icon">
+                        <p>
+                            <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                        </p>
+                    </div>
+                    <div class="slide-icon">
+                        <p>
+                            <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                        </p>
+                    </div>
+                    <div class="slide-icon">
+                        <p>
+                            <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                        </p>
+                    </div>
+                    <div class="slide-icon">
+                        <p>
+                            <span class="group-user-image" style="background: url('<?php echo base_url('assets/images/flags/ke.svg'); ?>');"></span>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <div class="col-sm-4 hidden-xs right-card switch-card chat-card">
@@ -43,7 +49,7 @@
             </div>
             <div class="chat-body"></div>
             <div class="input-group">
-                <span class="input-group-addon text-center;"><img src="<?php echo base_url('assets/images/the_emo.png'); ?>" class="the_emoji"/></span>
+                <span class="input-group-addon text-center;"><img src="<?php echo base_url('assets/images/the_emo.png'); ?>" class="the_emoji" /></span>
                 <input type="search" placeholder="Search groups by name" class="form-control input-lg" />
                 <span class="input-group-addon entypo-paper-plane"></span>
             </div>
