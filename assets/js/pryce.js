f5ad397a094a0ffeef25d3ab79b30d7761affe7b
Berjis <berjistechnologies@gmail.com>
2021-03-04 12:28:42 +0300

    MPesa

--- assets/js/pryce.js
+++ assets/js/pryce.js
@@ -107,6 +107,7 @@ $('.go_to_group').on('click', function () {
 })
 
 $('.g-switch').on('click', function () {
+    console.log('Show ' + $(this).attr('data-show'))
     $('.group-switched').hide();
     $('.' + $(this).attr('data-show')).show();
     $('.g-switch').removeClass('entypo-current');
