54e2f166431657195e256273e35643b001097d0b
Berjis <berjistechnologies@gmail.com>
2021-03-04 12:37:25 +0300

    MPesa

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -97,25 +97,25 @@
             <div class="row">
                 <div class="col-sm-12">
                     <div class="group-navi">
-                        <div class="g-switch group-entypo col-sm-3 col-xs-6 entypo-current" data-show="group-overview">
+                        <div class="group-entypo col-sm-3 col-xs-6 g-switch entypo-current" data-show="group-overview">
                             <div class="entypo-inner text-center">
                                 <span class="entypo-logout"></span>
                                 <span class="hidden-xs">Overview</span>
                             </div>
                         </div>
-                        <div class="g-switch group-entypo col-sm-3 col-xs-6" data-show="group-transacts">
+                        <div class="group-entypo col-sm-3 col-xs-6 g-switch" data-show="group-transacts">
                             <div class="entypo-inner text-center">
                                 <span class="entypo-doc-text"></span>
                                 <span class="hidden-xs">Statements</span>
                             </div>
                         </div>
-                        <div class="g-switch group-entypo col-sm-3 col-xs-6" data-show="group-topup">
+                        <div class="group-entypo col-sm-3 col-xs-6 g-switch" data-show="group-topup">
                             <div class="entypo-inner text-center">
                                 <span class="entypo-publish"></span>
                                 <span class="hidden-xs">Top Up</span>
                             </div>
                         </div>
-                        <div class="g-switch group-entypo col-sm-3 col-xs-6" data-show="group-more">
+                        <div class="group-entypo col-sm-3 col-xs-6 g-switch" data-show="group-more">
                             <div class="entypo-inner text-center">
                                 <span class="entypo-dot-3"></span>
                                 <span class="hidden-xs">More</span>
@@ -125,7 +125,7 @@
                 </div>
             </div>
 
-            <div class="group-swicthed row group-overview">
+            <div class="row group-swicthed group-overview">
                 <div class="col-sm-6 col-xs-12 text-center">
                     <div class="group-balance-preview">
                         <span class="gp-txt">Total balance</span>
@@ -151,7 +151,7 @@
                     </div>
                 </div>
             </div>
-            <div class="group-swicthed row group-transacts" style="display: none;">
+            <div class="row group-swicthed group-transacts" style="display: none;">
                 <table class="datatable" id="table-4">
                     <thead>
                         <tr>
@@ -177,10 +177,10 @@
                     </tbody>
                 </table>
             </div>
-            <div class="group-swicthed row group-topup" style="display: none;">
+            <div class="row group-swicthed group-topup" style="display: none;">
                 Top Up area here
             </div>
-            <div class="group-swicthed row group-more" style="display: none;">
+            <div class="row group-swicthed group-more" style="display: none;">
                 More acrtions here
             </div>
         </div>
