f57696d30a132a1d6a108b4ff41f9b8da6a0f82f
Berjis <berjistechnologies@gmail.com>
2021-03-07 18:48:24 +0300

    MPesa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
 	public function __construct()
 	{
 		if (!isset($_SESSION['sombo'])) {
-			return redirect()->to(site_url("https://radio.garden"));
+			redirect()->to(site_url("https://radio.garden"));
 		}
 	}
 	public function index()
