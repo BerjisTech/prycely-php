b2db62f3bcdd20fe1fc801986fd5a2d95c4c146d
Berjis <berjistechnologies@gmail.com>
2021-03-07 18:17:51 +0300

    MPesa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,12 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function __construct()
-    {
-        if(!isset($_SESSION['sombo'])){
-            header("location: https://radio.garden");
-        }
-    }
+	public function __construct()
+	{
+		if (!isset($_SESSION['sombo'])) {
+			header("location: https://radio.garden");
+		}
+	}
 	public function index()
 	{
 		return view('welcome_message');
@@ -22,6 +22,7 @@ class Home extends BaseController
 
 	public function dbtest()
 	{
+		print_r($this->db);
 		print_r($this->db->query('SELECT * FROM shops'));
 	}
 }
