11b41a41ff546f4627609d29095376d0b16fc334
Berjis <berjistechnologies@gmail.com>
2021-03-08 17:23:42 +0300

    new data tables

--- app/Views/group/group.php
+++ app/Views/group/group.php
@@ -96,7 +96,7 @@
             fileref.setAttribute("href", filename)
         }
         if (typeof fileref != "undefined")
-            document.getElementsByTagName("head")[0].appendChild(fileref)
+            document.getElementsByTagName("body")[0].appendChild(fileref)
     }
 </script>
 <div class="row">
