a381f17031f78eba65ba04f72d99fe6e36e9fda6
Berjis <berjistechnologies@gmail.com>
2021-03-09 21:20:21 +0300

    new data tables

--- app/Models/Mpesamodel.php
+++ app/Models/Mpesamodel.php
@@ -14,19 +14,24 @@ class Mpesamodel extends Model
         $url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        $credentials = base64_encode(env('MPESA_CONSUMER_KEY').':'.env('MPESA_CONSUMER_SECRET'));
+        $credentials = base64_encode(env('MPESA_CONSUMER_KEY') . ':' . env('MPESA_CONSUMER_SECRET'));
         curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic ' . $credentials)); //setting a custom header
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
         $curl_response = curl_exec($curl);
-        if ($curl_response != FALSE) {
-            $responseVals = json_decode($curl_response, TRUE);
-            $accessToken = $responseVals['access_token'];
-            $status = 1;
-        } else {
-            $description = "Curl Failed: " . curl_error($curl);
-        }
+
+        $responseVals = json_decode($curl_response, TRUE);
+        $accessToken = $responseVals['access_token'];
+        $status = 1;
+        
+        // if ($curl_response != FALSE) {
+        //     $responseVals = json_decode($curl_response, TRUE);
+        //     $accessToken = $responseVals['access_token'];
+        //     $status = 1;
+        // } else {
+        //     $description = "Curl Failed: " . curl_error($curl);
+        // }
         $array = array('status' => $status, 'token' => $accessToken, 'description' => $description);
         return json_encode($array);
     }
