489a19b38f424658a0cff8c588235b45358467cc
Berjis <berjistechnologies@gmail.com>
2021-04-03 18:53:35 +0300

    Create User

--- application/views/auth/onboarding.php
+++ application/views/auth/onboarding.php
@@ -7,10 +7,35 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/onboarding.css'); ?>" />
 </head>
 
 <body>
-
+    <div class="container">
+        <ul>
+            <li><span></span>
+                <div>
+                    <div class="title">Codify</div>
+                    <div class="info">Let&apos;s make coolest things in css</div>
+                    <div class="type">Presentation</div>
+                </div><span class="number"><span>10:00</span><span>12:00</span></span>
+            </li>
+            <li>
+                <div><span></span>
+                    <div class="title">Codify</div>
+                    <div class="info">Let&apos;s make coolest things in javascript</div>
+                    <div class="type">Presentation</div>
+                </div><span class="number"><span>13:00</span><span>14:00</span></span>
+            </li>
+            <li>
+                <div><span></span>
+                    <div class="title">Codify</div>
+                    <div class="info">Let&apos;s make coolest things in css</div>
+                    <div class="type">Review</div>
+                </div><span class="number"><span>15:00</span><span>17:45</span></span>
+            </li>
+        </ul>
+    </div>
 </body>
 
 </html>
