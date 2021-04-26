b5b430b3a2c0f82116aca9c1671794c5157ec829
Berjis <berjistechnologies@gmail.com>
2021-04-26 13:25:19 +0300

    Mpesa

--- application/controllers/Mpesa.php
+++ application/controllers/Mpesa.php
@@ -1,10 +1,13 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Mpesa extends CI_Controller
 {
+
+
     public function index()
     {
+        $mpesa= new \Safaricom\Mpesa\Mpesa();
     }
 }
