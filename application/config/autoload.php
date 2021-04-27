fe7a8d7150a6bdd32cb253a7a352a1939734a21a
Berjis <berjistechnologies@gmail.com>
2021-04-27 18:03:38 +0300

    composer

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require_once BASEPATH . '/helpers/url_helper.php';
+require_once BASEPATH . 'helpers/url_helper.php';
 //update
 
 /*
