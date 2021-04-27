3383afda4eedafbe0af08f29761726e4b0ef5820
Berjis <berjistechnologies@gmail.com>
2021-04-27 14:05:40 +0300

    routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$default_controller = "Welcome";
+$controller_exceptions = array('Auth','Createaccount', 'Group', 'Home', 'Internals', 'Mobilemoney', 'Mpesa', 'Newuser', 'Onboarding', 'Overview', 'p', 'Settings', 'Swift', 'Thecalls', 'Wallet');
+
+$route['default_controller'] = $default_controller;
+$route["^((?!\b".implode('\b|\b', $controller_exceptions)."\b).*)$"] = $default_controller.'/$1';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
