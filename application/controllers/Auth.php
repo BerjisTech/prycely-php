233f82b6cf71cab150e95641539409ca979ffb86
Berjis <berjistechnologies@gmail.com>
2021-04-27 14:13:10 +0300

    auth load url helper

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -8,6 +8,7 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+		$this->load->helper('url');
         /* cache control */
         $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
