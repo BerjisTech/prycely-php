45458fcab4e0d5b3921b34d6b2b351d4d0694374
Berjis <berjistechnologies@gmail.com>
2021-04-27 14:20:45 +0300

    remove cache control

--- application/controllers/Createaccount.php
+++ application/controllers/Createaccount.php
@@ -8,11 +8,7 @@ class Createaccount extends CI_Controller
     {
         parent::__construct();
         /* cache control */
-        $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
-        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
-        $this->output->set_header('Pragma: no-cache');
-        $this->output->set_header("Expires: Mon, 26 Jul 2020 05:00:00 GMT");
-        date_default_timezone_set("Africa/Nairobi");
+        
     }
 
     public function index()
