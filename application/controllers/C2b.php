7a3a49112c70de68370a80251701c50855e4e559
Berjis <berjistechnologies@gmail.com>
2021-04-28 15:25:46 +0300

    Paybill

--- application/controllers/C2b.php
+++ application/controllers/C2b.php
@@ -68,6 +68,35 @@ class C2b extends CI_Controller
 
     private function phoneFormat($phone)
     {
-        return $phone;
+        //initialize valuables
+        $status = FALSE;
+        $formattedPhone = '';
+        //remove white spaces
+        $phone = trim($phone);
+        $phone = str_replace(" ", "", $phone);
+        //remove -, (, and )
+        $phone = str_replace("-", "", $phone);
+        $phone = str_replace("(", "", $phone);
+        $phone = str_replace(")", "", $phone);
+        //validate - all should begin with 254
+        if (strlen($phone) >= 9 && strlen($phone) <= 13) {
+            if (substr($phone, 0, 2) == "07") {
+                $phone = substr_replace($phone, "254", 0, 1);
+            } elseif (substr($phone, 0, 4) == "+254") {
+                $phone = substr_replace($phone, "", 0, 1);
+            } elseif (substr($phone, 0, 1) === "7") {
+                $phone = substr_replace($phone, "254", 0, 0);
+            } elseif (substr($phone, 0, 1) === "1") {
+                $phone = substr_replace($phone, "254", 0, 0);
+            } elseif (substr($phone, 0, 2) == "01") {
+                $phone = substr_replace($phone, "254", 0, 1);
+            }
+            if (substr($phone, 0, 3) == "254" && strlen($phone) == 12 && is_numeric($phone)) {
+                $status = TRUE;
+                $formattedPhone = $phone;
+            }
+        }
+        $array = array('status' => $status, 'formattedPhone' => $formattedPhone);
+        return json_encode($array);
     }
 }
