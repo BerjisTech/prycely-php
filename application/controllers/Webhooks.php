11cf28ba8f5e680e81b8c5e61c9a54faa8d42ed0
Berjis <berjistechnologies@gmail.com>
2021-04-29 00:03:12 +0300

    Paybill

--- application/controllers/Webhooks.php
+++ application/controllers/Webhooks.php
@@ -8,6 +8,12 @@ class Webhooks extends CI_Controller
     {
         if ($_POST['payload']) {
             shell_exec('cd /var/www/prycely && git reset –hard HEAD && git pull');
+            $this->Email->do_email(
+                'New pull from Git<br />Pull Data<br /><br />' . $this->input->post(),
+                'New pull from Git',
+                'prycely@gmail.com, berjistechnologies@gmail.com',
+                'support@sleekupsell.com'
+            );
         }
     }
 }
