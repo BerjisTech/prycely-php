e16672356cc4f65c00c82241db86eb86b1e9b88d
Berjis <berjistechnologies@gmail.com>
2021-04-29 00:09:55 +0300

    Paybill

--- application/controllers/Webhooks.php
+++ application/controllers/Webhooks.php
@@ -6,6 +6,30 @@ class Webhooks extends CI_Controller
 {
     public function index()
     {
+    }
+
+    public function git_pull($sabanduklo)
+    {
+        if (!isset($sabanduklo)) {
+            $this->Email->do_email(
+                'Some tried accessing sabanduklo on ' . date('d M, Y', time()) . 'at ' . date('H:m:s', time()),
+                'Funny call',
+                'prycely@gmail.com, berjistechnologies@gmail.com',
+                'support@sleekupsell.com'
+            );
+            exit();
+        }
+
+        if ($sabanduklo != 'sabanduklobankulo') {
+            $this->Email->do_email(
+                'Some tried accessing sabanduklo using ' . $sabanduklo . ' on ' . date('d M, Y', time()) . 'at ' . date('H:m:s', time()),
+                'Funny call',
+                'prycely@gmail.com, berjistechnologies@gmail.com',
+                'support@sleekupsell.com'
+            );
+            exit();
+        }
+
         if ($_POST['payload']) {
             shell_exec('cd /var/www/prycely && git reset –hard HEAD && git pull');
             $this->Email->do_email(
@@ -14,6 +38,14 @@ class Webhooks extends CI_Controller
                 'prycely@gmail.com, berjistechnologies@gmail.com',
                 'support@sleekupsell.com'
             );
+        } else {
+            $this->Email->do_email(
+                'Some tried accessing sabanduklo without a post request on ' . date('d M, Y', time()) . 'at ' . date('H:m:s', time()),
+                'Funny call',
+                'prycely@gmail.com, berjistechnologies@gmail.com',
+                'support@sleekupsell.com'
+            );
+            exit();
         }
     }
 }
