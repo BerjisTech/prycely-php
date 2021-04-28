ac145d67153973dd54d105ee6939d45800b14393
Berjis <berjistechnologies@gmail.com>
2021-04-28 14:11:07 +0300

    Paybill

--- application/controllers/Mpesa.php
+++ application/controllers/Mpesa.php
@@ -174,6 +174,7 @@ class Mpesa extends CI_Controller
     {
         try {
             $request = file_get_contents('php://input');
+            print_r($request);
             $this->db->insert('errors', array('error' => json_encode($request)));
 
             //when success
