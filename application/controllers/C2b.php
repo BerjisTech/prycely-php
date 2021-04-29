c8850702d8c1bac0951d49c0ab94c928de8f6478
Berjis <berjistechnologies@gmail.com>
2021-04-29 21:35:43 +0300

    Transactions

--- application/controllers/C2b.php
+++ application/controllers/C2b.php
@@ -82,10 +82,13 @@ class C2b extends CI_Controller
         }
 
         $transaction = $this->db->where('the_transaction_reference', $MpesaCode)->get('the_transactions')->row();
+        
         $user_id = $transaction->the_transaction_user;
         $wallet = $transaction->the_transaction_wallet;
-        $new_total = $this->db->select('SUM(amount) as total')->where('the_transaction_user', $user_id)->where('the_transaction_status', 1)->get('the_transactions')->row()->total;
-        $pending_total = $this->db->select('SUM(amount) as total')->where('the_transaction_user', $user_id)->where('the_transaction_status', 2)->get('the_transactions')->row()->total;
+
+        $new_total = $this->db->select('SUM(the_transaction_amount) as total')->where('the_transaction_user', $user_id)->where('the_transaction_status', 1)->get('the_transactions')->row()->total;
+        $pending_total = $this->db->select('SUM(the_transaction_amount) as total')->where('the_transaction_user', $user_id)->where('the_transaction_status', 2)->get('the_transactions')->row()->total;
+        
         $this->db->where('the_wallet_user', $user_id)->where('the_wallet_id', $wallet)->set('the_wallet_balance', $new_total)->update('the_wallets');
         $this->db->where('the_wallet_user', $user_id)->where('the_wallet_id', $wallet)->set('the_wallet_pending', $pending_total)->update('the_wallets');
     }
