374b412b38d85ecfd79222f6c179a0db3627a6e9
Berjis <berjistechnologies@gmail.com>
2021-04-29 21:13:51 +0300

    Transactions

--- application/controllers/C2b.php
+++ application/controllers/C2b.php
@@ -80,6 +80,12 @@ class C2b extends CI_Controller
             );
             $this->db->insert('the_transactions', $currentTrans);
         }
+
+        $transaction = $this->db->where('the_transaction_reference', $MpesaCode)->get('the_transactions')->row();
+        $user_id = $transaction->the_transaction_user;
+        $wallet = $transaction->the_transaction_wallet;
+        $new_total = $this->db->select('SUM(amount) as total')->where('the_transaction_user', $user_id)->where('the_transaction_status', 1)->get('the_transactions')->row()->total;
+        $this->db->where('the_wallet_user', $user_id)->where('the_wallet_id', $wallet)->set('amount', $new_total)->update('the_wallets');
     }
 
     private function phoneFormat($phone)
