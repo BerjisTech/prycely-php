8d4fc0a6589cf4a7934bbbc61c3552de60728990
Berjis <berjistechnologies@gmail.com>
2021-04-29 20:41:26 +0300

    Transactions

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -27,7 +27,7 @@ class Overview extends CI_Controller
 
         foreach ($dates as $date) {
             $transactions[$date['the_transaction_date']] = $this->db
-                ->query('SELECT * FROM `the_transactions` WHERE `the_transaction_user` = 1 AND date_format(from_unixtime(the_transaction_date), "%d") = ' . date('d', $date['the_transaction_date']))->result_array();
+                ->query('SELECT * FROM `the_transactions` ORDER BY `the_transaction_id` DESC WHERE `the_transaction_user` = 1 AND date_format(from_unixtime(the_transaction_date), "%d") = ' . date('d', $date['the_transaction_date']))->result_array();
         }
 
         $data['transactions'] = $transactions;
