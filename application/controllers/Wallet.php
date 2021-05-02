fa2b3160e0709300f884a543d5420940ded65280
berjis <berjistechnologies@gmail.com>
2021-05-02 23:13:26 +0300

    Wallet

--- application/controllers/Wallet.php
+++ application/controllers/Wallet.php
@@ -100,6 +100,7 @@ class Wallet extends CI_Controller
 	{
 		$data['wallet'] = $this->db->where('the_wallet_id', $wallet_id)->get('the_wallets')->row();
 		$data['wallets'] = $this->db->where('the_wallet_user', $this->session->the_person_id)->get('the_wallets')->result_array();
+		$data['user_details'] = $this->Database->select_single('the_person_first_name, the_person_last_name', array('the_person_email' => $this->session->the_person_email), NULL, 'the_people');
 		$data['page_name'] = 'wallet/wallet';
 		$data['page_title'] = 'Wallet';
 
