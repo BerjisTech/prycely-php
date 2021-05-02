d8786c45d802adda0579b09e9184169958e56fc6
berjis <berjistechnologies@gmail.com>
2021-05-02 23:27:48 +0300

    Wallet

--- application/controllers/Wallet.php
+++ application/controllers/Wallet.php
@@ -98,6 +98,23 @@ class Wallet extends CI_Controller
 
 	public function view($wallet_id)
 	{
+		$dates = $this->db
+			->select('the_transaction_date')
+			->where('the_transaction_user', $this->session->the_person_id)
+			->group_by('Day(the_transaction_date)')
+			->limit('10')
+			->order_by('the_transaction_date', 'DESC')
+			->get('the_transactions')->result_array();
+
+		foreach ($dates as $date) {
+			$collection_date = date('dmY', $date['the_transaction_date']);
+			$collection_stamp = date('j\<\s\u\p\>S\<\/\s\u\p\> M', $date['the_transaction_date']);
+			$transactions[$collection_stamp] = $this->db
+				->query("SELECT * FROM `the_transactions` WHERE `the_transaction_user` = 1 AND WHERE `the_transaction_wallet` = $wallet_id AND date_format(from_unixtime(the_transaction_date), '%d%m%Y') = $collection_date ORDER BY `the_transaction_id` DESC LIMIT 10")->result_array();
+		}
+
+		$data['transactions'] = $transactions;
+
 		$data['wallet'] = $this->db->where('the_wallet_id', $wallet_id)->get('the_wallets')->row();
 		$data['wallets'] = $this->db->where('the_wallet_user', $this->session->the_person_id)->get('the_wallets')->result_array();
 		$data['user_details'] = $this->Database->select_single('the_person_first_name, the_person_last_name', array('the_person_email' => $this->session->the_person_email), NULL, 'the_people');
--- application/views/wallet/wallet.php
+++ application/views/wallet/wallet.php
@@ -44,20 +44,63 @@
 		</div>
 	</div>
 	<div class="col-sm-4">
-		<div class="exchange-rates">
-			<div class="exchange-top">
-				<span>Exchange Rates</span>
-				<span><span class="entypo-record savings"></span> KES</span>
-				<span><span class="entypo-record expenses"></span> USD</span>
-			</div>
-			<div id="line-chart" class="morrischart" style="height: 300px; position: relative;"></div>
-		</div>
+		<span>My Wallet balance</span>
+		<span><?php echo $wallet->the_wallet_currency . ' ' . $wallet->the_wallet_balance; ?></span>
+
 	</div>
 </div>
 
 <div class="row">
-	<div class="col-sm-6"></div>
-	<div class="col-sm-6"></div>
+	<div class="col-sm-6">
+		<?php foreach ($transactions as $key => $date) : ?>
+			<span>
+				<?php
+				if ($key == date('j\<\s\u\p\>S\<\/\s\u\p\> M', time())) {
+					echo 'Today';
+				} else if ($key == date('j\<\s\u\p\>S\<\/\s\u\p\> M', strtotime("-1 days"))) {
+					echo 'Yesterday';
+				} else {
+					echo $key;
+				}
+				?>
+			</span>
+			<table class="table transaction-table table-hover">
+				<?php foreach ($date as $transaction) : ?>
+					<tr>
+						<td class="transaction-details">
+							<span class="transaction-title"><?php echo $transaction['the_transaction_purpose']; ?></span>
+							<?php if ($transaction['the_transaction_status'] == 1) : ?>
+								<span class="transaction-status complete">
+									complete
+								</span>
+							<?php else : ?>
+								<span class="transaction-status">
+									failed
+								</span>
+							<?php endif; ?>
+						</td>
+						<td class="transaction-td">
+							<span class="transaction-amount <?php if ($transaction['the_transaction_type'] == 1) {
+																echo "complete";
+															} ?>"><?php echo $transaction['the_transaction_currency'] . ' ' . number_format($transaction['the_transaction_amount']) ?></span>
+							<span class="transaction-time"><?php echo date('h:i:s a', $transaction['the_transaction_date']); ?></span>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</table>
+			<hr />
+		<?php endforeach; ?>
+	</div>
+	<div class="col-sm-6">
+		<div class="exchange-rates">
+			<div class="exchange-top">
+				<span>Transaction Overview</span>
+				<span><span class="entypo-record in"></span> Money in</span>
+				<span><span class="entypo-record out"></span> Money out</span>
+			</div>
+			<div id="line-chart" class="morrischart" style="height: 300px; position: relative;"></div>
+		</div>
+	</div>
 </div>
 
 
