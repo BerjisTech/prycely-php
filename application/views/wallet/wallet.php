8bc3314354ca05476f40c2279bbcc7ed3755c27c
berjis <berjistechnologies@gmail.com>
2021-05-02 23:21:23 +0300

    Wallet

--- application/views/wallet/wallet.php
+++ application/views/wallet/wallet.php
@@ -1,67 +1,3 @@
-<script>
-	jQuery(document).ready(function($) {
-
-		var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-		// Line Charts
-		var line_chart_demo = $("#line-chart");
-		var line_chart = Morris.Line({
-			element: 'line-chart',
-			data: [
-				<?php for ($m = 7; $m > -1; $m--) : ?> {
-						y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
-						a: getRandomInt(10000, 1000),
-						b: getRandomInt(10000, 1000)
-					},
-				<?php endfor; ?>
-			],
-			xkey: 'y',
-			ykeys: ['a', 'b'],
-			labels: ['Deposits', 'Withdrawals'],
-			lineColors: ['#ec3b83', '#E8B51B', '#00acd6'],
-			xLabelFormat: function(d) {
-				return d.getDate() + ' ' + months[d.getMonth()];
-			},
-			dateFormat: function(x) {
-				let shit = new Date(x);
-				var douche = shit.getDate() + ' ' + months[shit.getMonth()];
-				return douche;
-			},
-			resize: true,
-			smooth: true,
-			pointSize: 0,
-			redraw: true
-		});
-		line_chart_demo.parent().attr('style', '');
-
-		// Donut Chart
-		var donut_chart_demo = $("#donut-chart");
-		donut_chart_demo.parent().show();
-		var donut_chart = Morris.Donut({
-			element: 'donut-chart',
-			data: [{
-					label: "Member Deposits",
-					value: getRandomInt(10, 50)
-				},
-				{
-					label: "Project Expenses",
-					value: getRandomInt(10, 50)
-				},
-				{
-					label: "Withdrawals & Refunds",
-					value: getRandomInt(10, 50)
-				}
-			],
-			colors: ['#EC3B83', '#00ACD6', '#E8B51B']
-		});
-		donut_chart_demo.parent().attr('style', '');
-
-	});
-
-	function getRandomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-</script>
-
 <div class="row">
 	<div class="col-sm-8">
 		<p class="">Transfer to your other wallets</p>
@@ -123,3 +59,46 @@
 	<div class="col-sm-6"></div>
 	<div class="col-sm-6"></div>
 </div>
+
+
+<script>
+	jQuery(document).ready(function($) {
+
+		var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+		// Line Charts
+		var line_chart_demo = $("#line-chart");
+		var line_chart = Morris.Line({
+			element: 'line-chart',
+			data: [
+				<?php for ($m = 7; $m > -1; $m--) : ?> {
+						y: '<?php echo date('Y-m-d', strtotime('-' . $m . ' days')); ?>',
+						a: getRandomInt(10000, 1000),
+						b: getRandomInt(10000, 1000)
+					},
+				<?php endfor; ?>
+			],
+			xkey: 'y',
+			ykeys: ['a', 'b'],
+			labels: ['Deposits', 'Withdrawals'],
+			lineColors: ['#ec3b83', '#E8B51B', '#00acd6'],
+			xLabelFormat: function(d) {
+				return d.getDate() + ' ' + months[d.getMonth()];
+			},
+			dateFormat: function(x) {
+				let shit = new Date(x);
+				var douche = shit.getDate() + ' ' + months[shit.getMonth()];
+				return douche;
+			},
+			resize: true,
+			smooth: true,
+			pointSize: 0,
+			redraw: true
+		});
+		line_chart_demo.parent().attr('style', '');
+
+	});
+
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+</script>
