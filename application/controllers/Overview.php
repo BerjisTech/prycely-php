7233dc6ff246ee406cbbcc8d731a4a9103ac0ba4
berjis <berjistechnologies@gmail.com>
2021-05-02 23:47:05 +0300

    Wallet

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -20,7 +20,7 @@ class Overview extends CI_Controller
         $dates = $this->db
 			->select('the_transaction_date')
 			->where('the_transaction_user', $this->session->the_person_id)
-			->group_by('Day(the_transaction_date)')
+			->group_by('date_format(from_unixtime(the_transaction_date), "%d")')
 			->limit('10')
 			->order_by('the_transaction_date', 'DESC')
 			->get('the_transactions')->result_array();
