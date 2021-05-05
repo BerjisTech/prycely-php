7679f40a47379c60a323f035ff9a4d3cb856a4ca
berjis <berjistechnologies@gmail.com>
2021-05-05 16:17:58 +0300

    Wallet

--- application/views/wallet/wallet.php
+++ application/views/wallet/wallet.php
@@ -1,103 +1,111 @@
 <div class="row">
-	<div class="col-sm-8 panelCard">
-		<p class="transfer-title">Transfer to your other wallets</p>
-		<div class="transfer-from-this">
-			<div class="transfer-amount-details">
-				<span>You send</span>
-				<input type="number" value="1000" />
+	<div class="col-sm-8">
+		<div class="panelCard">
+			<p class="transfer-title">Transfer to your other wallets</p>
+			<div class="transfer-from-this">
+				<div class="transfer-amount-details">
+					<span>You send</span>
+					<input type="number" value="1000" />
+				</div>
+				<div class="transfer-wallet-details">
+					<img src="<?php echo base_url('assets/images/flags/') . strtolower(substr($wallet->the_wallet_currency, 0, 2)); ?>.svg" />
+					<span><?php echo $wallet->the_wallet_currency; ?></span>
+					<span class="entypo-down-open" style="display: hidden;"></span>
+				</div>
 			</div>
-			<div class="transfer-wallet-details">
-				<img src="<?php echo base_url('assets/images/flags/') . strtolower(substr($wallet->the_wallet_currency, 0, 2)); ?>.svg" />
-				<span><?php echo $wallet->the_wallet_currency; ?></span>
-				<span class="entypo-down-open" style="display: hidden;"></span>
+			<div class="transfer-to-this">
+				<div class="transfer-amount-details">
+					<span>They receive</span>
+					<input type="number" value="1000" />
+				</div>
+				<div class="transfer-wallet-details">
+					<img src="<?php echo base_url('assets/images/flags/') . strtolower(substr($wallets[0]['the_wallet_currency'], 0, 2)); ?>.svg" />
+					<span><?php echo $wallets[0]['the_wallet_currency']; ?></span>
+					<span class="entypo-down-open"></span>
+				</div>
+				<div class="currency-drop">
+					<?php foreach ($wallets as $fetch) : ?>
+						<div class="currency-select">
+							<img src="<?php echo base_url('assets/images/flags/') . strtolower(substr($fetch['the_wallet_currency'], 0, 2)); ?>.svg" />
+							<span><?php echo $fetch['the_wallet_currency']; ?></span>
+						</div>
+					<?php endforeach; ?>
+				</div>
 			</div>
-		</div>
-		<div class="transfer-to-this">
-			<div class="transfer-amount-details">
-				<span>They receive</span>
-				<input type="number" value="1000" />
+			<div class="transfer-bottom">
+				<div class="converted-amount">
+					<span>Amount we'll convert</span>
+					<span>1,000 KES</span>
+				</div>
+				<div class="conversion-fees">
+					<span>Total fees</span>
+					<span>1,000 KES</span>
+				</div>
+				<button>SEND MONEY</button>
 			</div>
-			<div class="transfer-wallet-details">
-				<img src="<?php echo base_url('assets/images/flags/') . strtolower(substr($wallets[0]['the_wallet_currency'], 0, 2)); ?>.svg" />
-				<span><?php echo $wallets[0]['the_wallet_currency']; ?></span>
-				<span class="entypo-down-open"></span>
-			</div>
-			<div class="currency-drop">
-				<?php foreach ($wallets as $fetch) : ?>
-					<div class="currency-select">
-						<img src="<?php echo base_url('assets/images/flags/') . strtolower(substr($fetch['the_wallet_currency'], 0, 2)); ?>.svg" />
-						<span><?php echo $fetch['the_wallet_currency']; ?></span>
-					</div>
-				<?php endforeach; ?>
-			</div>
-		</div>
-		<div class="transfer-bottom">
-			<div class="converted-amount">
-				<span>Amount we'll convert</span>
-				<span>1,000 KES</span>
-			</div>
-			<div class="conversion-fees">
-				<span>Total fees</span>
-				<span>1,000 KES</span>
-			</div>
-			<button>SEND MONEY</button>
 		</div>
 	</div>
-	<div class="col-sm-4 panelCard">
-		<span>My Wallet balance</span>
-		<span><?php echo $wallet->the_wallet_currency . ' ' . $wallet->the_wallet_balance; ?></span>
+	<div class="col-sm-4">
+		<div class="panelCard">
+			<span>My Wallet balance</span>
+			<span><?php echo $wallet->the_wallet_currency . ' ' . $wallet->the_wallet_balance; ?></span>
+		</div>
 	</div>
 </div>
 
 <div class="row">
-	<div class="col-sm-6 panelCard">
-		<?php foreach ($transactions as $key => $date) : ?>
-			<span>
-				<?php
-				if ($key == date('j\<\s\u\p\>S\<\/\s\u\p\> M', time())) {
-					echo 'Today';
-				} else if ($key == date('j\<\s\u\p\>S\<\/\s\u\p\> M', strtotime("-1 days"))) {
-					echo 'Yesterday';
-				} else {
-					echo $key;
-				}
-				?>
-			</span>
-			<table class="table transaction-table table-hover">
-				<?php foreach ($date as $transaction) : ?>
-					<tr>
-						<td class="transaction-details">
-							<span class="transaction-title"><?php echo $transaction['the_transaction_purpose']; ?></span>
-							<?php if ($transaction['the_transaction_status'] == 1) : ?>
-								<span class="transaction-status complete">
-									complete
-								</span>
-							<?php else : ?>
-								<span class="transaction-status">
-									failed
-								</span>
-							<?php endif; ?>
-						</td>
-						<td class="transaction-td">
-							<span class="transaction-amount <?php if ($transaction['the_transaction_type'] == 1) {
-																echo "complete";
-															} ?>"><?php echo $transaction['the_transaction_currency'] . ' ' . number_format($transaction['the_transaction_amount']) ?></span>
-							<span class="transaction-time"><?php echo date('h:i:s a', $transaction['the_transaction_date']); ?></span>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-			</table>
-			<hr />
-		<?php endforeach; ?>
+	<div class="col-sm-6">
+		<div class="panelCard">
+			<?php foreach ($transactions as $key => $date) : ?>
+				<span>
+					<?php
+					if ($key == date('j\<\s\u\p\>S\<\/\s\u\p\> M', time())) {
+						echo 'Today';
+					} else if ($key == date('j\<\s\u\p\>S\<\/\s\u\p\> M', strtotime("-1 days"))) {
+						echo 'Yesterday';
+					} else {
+						echo $key;
+					}
+					?>
+				</span>
+				<table class="table transaction-table table-hover">
+					<?php foreach ($date as $transaction) : ?>
+						<tr>
+							<td class="transaction-details">
+								<span class="transaction-title"><?php echo $transaction['the_transaction_purpose']; ?></span>
+								<?php if ($transaction['the_transaction_status'] == 1) : ?>
+									<span class="transaction-status complete">
+										complete
+									</span>
+								<?php else : ?>
+									<span class="transaction-status">
+										failed
+									</span>
+								<?php endif; ?>
+							</td>
+							<td class="transaction-td">
+								<span class="transaction-amount <?php if ($transaction['the_transaction_type'] == 1) {
+																	echo "complete";
+																} ?>"><?php echo $transaction['the_transaction_currency'] . ' ' . number_format($transaction['the_transaction_amount']) ?></span>
+								<span class="transaction-time"><?php echo date('h:i:s a', $transaction['the_transaction_date']); ?></span>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+				</table>
+				<hr />
+			<?php endforeach; ?>
+		</div>
 	</div>
-	<div class="col-sm-6 panelCard">
-		<div class="exchange-rates">
-			<div class="exchange-top">
-				<span>Transaction Overview</span>
-				<span><span class="entypo-record in"></span> Money in</span>
-				<span><span class="entypo-record out"></span> Money out</span>
+	<div class="col-sm-6">
+		<div class="panelCard">
+			<div class="exchange-rates">
+				<div class="exchange-top">
+					<span>Transaction Overview</span>
+					<span><span class="entypo-record in"></span> Money in</span>
+					<span><span class="entypo-record out"></span> Money out</span>
+				</div>
+				<div id="line-chart" class="morrischart" style="height: 300px; position: relative;"></div>
 			</div>
-			<div id="line-chart" class="morrischart" style="height: 300px; position: relative;"></div>
 		</div>
 	</div>
 </div>
