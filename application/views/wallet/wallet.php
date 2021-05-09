889782e690baf82ddbae7a246728b7d1b1314eb4
berjis <berjistechnologies@gmail.com>
2021-05-10 02:00:15 +0300

    Wallet TopUp

--- application/views/wallet/wallet.php
+++ application/views/wallet/wallet.php
@@ -243,7 +243,7 @@
 				<div class="col-sm-3"></div>
 			</div>
 		</div>
-		<div class="row payChoicePanel">
+		<div class="panelCard payChoicePanel">
 			<div class="col-sm-8">
 				<div class="payChoice pc-mPesa">
 					<span class="entypo-credit-card"></span>
