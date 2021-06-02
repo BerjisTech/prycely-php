cf806f5ba2a245b6e847f7e1e5375135d9c80858
berjis <berjistechnologies@gmail.com>
2021-06-03 00:07:28 +0300

    Add Flutterwave

--- application/controllers/Mpesa.php
+++ application/controllers/Mpesa.php
@@ -150,7 +150,7 @@ class Mpesa extends CI_Controller
 
             $currentTrans = array(
                 'the_transaction_id' => '',
-                'the_transaction_user' => $user,
+                'the_transaction_user' => $this->session->the_person_id,
                 'the_transaction_date' => time(),
                 'the_transaction_start' => time(),
                 'the_transaction_end' => time(),
