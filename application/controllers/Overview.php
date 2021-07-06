ac4193e210d240382048a5f28d6037854fbe6dec
berjis <berjistechnologies@gmail.com>
2021-07-06 15:01:10 +0300

    Overview Model

--- application/controllers/Overview.php
+++ application/controllers/Overview.php
@@ -21,42 +21,17 @@ class Overview extends CI_Controller
 
     public function index()
     {
-		$me = $this->session->the_person_id;
-        $dates = $this->db
-            ->select('the_transaction_date')
-            ->where('the_transaction_user', $this->session->the_person_id)
-            ->where('the_transaction_status !=', 2)
-            ->group_by('date_format(from_unixtime(the_transaction_date), "%d")')
-            ->limit('10')
-            ->order_by('the_transaction_date', 'DESC')
-            ->get('the_transactions')->result_array();
+        $this->load->model('Overview_model');
 
-        $transactions = array();
-
-        foreach ($dates as $date) {
-            $collection_date = date('dmY', $date['the_transaction_date']);
-            $collection_stamp = date('j\<\s\u\p\>S\<\/\s\u\p\> M', $date['the_transaction_date']);
-            $transactions[$collection_stamp] = $this->db
-                ->query("SELECT * FROM `the_transactions` WHERE `the_transaction_user` = $me AND date_format(from_unixtime(the_transaction_date), '%d%m%Y') = $collection_date AND `the_transaction_status` != 2 ORDER BY `the_transaction_id` DESC LIMIT 10")->result_array();
-        }
+        $me = $this->session->the_person_id;
 
         $data['groups'] = array();
-
-        $my_groups = $this->db->select('the_person_groups')->where('the_person_id', $this->session->the_person_id)->get('the_people')->row()->the_person_groups;
-        if ($my_groups != '') {
-            $my_groups =  explode(',', $my_groups);
-            if (count($my_groups) > 0 || sizeof($my_groups) > 0) {
-                foreach ($my_groups as $key => $group) {
-                    $data['groups'][$key] = $this->db->where('the_group_id', $group)->get('the_groups')->result_array()[0];
-                }
-            }
-        }
-
-        $data['transactions'] = $transactions;
+        $data['groups'] = $this->Overview_model->get_groups(5, 0, $me);
+        $data['transactions'] = $this->Overview_model->get_transactions($me);
         $data['page_name'] = 'overview/index';
         $data['page_title'] = 'Overview';
         $data['user_details'] = $this->Database->select_single('the_person_first_name, the_person_last_name', array('the_person_email' => $this->session->the_person_email), NULL, 'the_people');
-        $data['wallets'] = $this->db->where('the_wallet_user', $this->session->the_person_id)->limit('3')->get('the_wallets')->result_array();
+        $data['wallets'] = $this->db->where('the_wallet_user', $me)->limit('3')->get('the_wallets')->result_array();
 
         $this->load->view('index', $data);
     }
---
+++ application/models/Overview_model.php
@@ -0,0 +1,44 @@
+<?php
+
+class Overview_model extends CI_Model
+{
+    public function get_groups($limit, $offset, $user)
+    {
+        $groups = $this
+            ->db
+            ->where('the_user_id', $this->session->the_person_id)
+            ->where('the_member_status', 1)
+            ->join('the_groups', 'the_groups.the_group_id = the_group_members.the_group_id')
+            ->get('the_group_members')
+            ->result_array();
+
+        foreach ($groups as $key => $group) {
+            $group[$key]['the_transaction_amount'] = $this->db->select('SUM(the_transaction_amount) so_far')->where('the_transaction_group', $group['the_group_id'])->get('the_transactions')->row()->so_far;
+        }
+
+        return $groups;
+    }
+
+    public function get_transactions($user)
+    {
+        $dates = $this->db
+            ->select('the_transaction_date')
+            ->where('the_transaction_user', $user)
+            ->where('the_transaction_status !=', 2)
+            ->group_by('date_format(from_unixtime(the_transaction_date), "%d")')
+            ->limit('10')
+            ->order_by('the_transaction_date', 'DESC')
+            ->get('the_transactions')->result_array();
+
+        $transactions = array();
+
+        foreach ($dates as $date) {
+            $collection_date = date('dmY', $date['the_transaction_date']);
+            $collection_stamp = date('j\<\s\u\p\>S\<\/\s\u\p\> M', $date['the_transaction_date']);
+            $transactions[$collection_stamp] = $this->db
+                ->query("SELECT * FROM `the_transactions` WHERE `the_transaction_user` = $user AND date_format(from_unixtime(the_transaction_date), '%d%m%Y') = $collection_date AND `the_transaction_status` != 2 ORDER BY `the_transaction_id` DESC LIMIT 10")->result_array();
+        }
+
+        return $transactions;
+    }
+}
